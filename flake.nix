f6a596e27d2818697abc00e68f6bf6825ae0cfce
Mr Z <denver.cfman@gmail.com>
2026-06-23 20:19:10 -0600

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,14 +2,29 @@
   description = "NixOS based netboot micro cluster, based on RPi Zero 2 W and RPi 4 devices";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    uboot-builder.url = "github:denver-cfman/nixos-netboot-cluster-uboot"; 
+    uboot-builder.url = "github:denver-cfman/nixos-netboot-cluster-uboot";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, uboot-builder }: {
+  outputs = { self, nixpkgs, uboot-builder, disko, sops-nix, home-manager }: {
     nixosConfigurations = {
       netboot-server = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [ 
+        modules = [
+          disko.nixosModules.disko
+          sops-nix.nixosModules.sops
+          hermes-agent.nixosModules.default
           ./hosts/netboot-server.nix
           ./modules/netboot-infrastructure.nix
         ];
