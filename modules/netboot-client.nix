f4db3904e0cdc2950a7e04f6ee7c89430fe70f65
Mr Z <denver.cfman@gmail.com>
2026-06-23 19:13:56 -0600

    Add NFS configuration for netboot client

--- modules/netboot-client.nix
+++ modules/netboot-client.nix
@@ -1,1 +1,12 @@
-{}
+# modules/netboot-client.nix
+{ config, lib, ... }: {
+  fileSystems."/" = {
+    device = "192.168.1.5:/export/client-01";
+    fsType = "nfs";
+    options = [ "vers=4.2" "proto=tcp" "timeo=600" ];
+  };
+  
+  # Ensure the kernel has NFS support built-in
+  boot.initrd.supportedFilesystems = [ "nfs" ];
+  boot.kernelParams = [ "nfsroot=192.168.1.5:/export/client-01" "ip=dhcp" ];
+}
