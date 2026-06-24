ff34e29869a0610bc1e0bbb13d912cef1ef29814
Mr Z <denver.cfman@gmail.com>
2026-06-23 19:08:16 -0600

    Add initial flake.nix configuration for NixOS

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Point to your existing U-Boot flake here
+    uboot-builder.url = "path:./uboot-repo"; 
+  };
+
+  outputs = { self, nixpkgs, uboot-builder }: {
+    nixosConfigurations = {
+      # The Netboot Server
+      netboot-server = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./hosts/netboot-server.nix ];
+      };
+      
+      # The First Client
+      pi-client-01 = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [ ./hosts/pi-client-01.nix ];
+      };
+    };
+  };
+}
