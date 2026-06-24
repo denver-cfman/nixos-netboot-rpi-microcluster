d5b372f52dff31837a279226752986024ef503a0
Mr Z <denver.cfman@gmail.com>
2026-06-23 20:04:06 -0600

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,4 +1,5 @@
 {
+  description = "NixOS based netboot micro cluster, based on RPi Zero 2 W and RPi 4 devices";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     uboot-builder.url = "github:denver-cfman/nixos-netboot-cluster-uboot"; 
