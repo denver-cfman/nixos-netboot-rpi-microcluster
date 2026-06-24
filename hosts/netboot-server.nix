3027ef74385527b9c601094446ddcc907cb58bc4
Mr Z <denver.cfman@gmail.com>
2026-06-23 20:08:46 -0600

    Initialize netboot server configuration

--- hosts/netboot-server.nix
+++ hosts/netboot-server.nix
@@ -1,1 +1,13 @@
-{}
+{ config, pkgs, ... }:
+
+{
+  imports = [ ../modules/netboot-infrastructure.nix ];
+
+  cluster.netboot.rootDir = "/export";
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/tftpboot 0755 root root -"
+    "d /export 0755 root root -"
+    "d /export/pi-client-01 0755 root root -"
+  ];
+}
