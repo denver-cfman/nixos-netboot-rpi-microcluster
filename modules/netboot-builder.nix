226abb6281a54daadbcf2998be45b5c3ef8c3568
Mr Z <denver.cfman@gmail.com>
2026-06-24 18:50:05 -0600

    Update mkimage tool path to use buildPackages

--- modules/netboot-builder.nix
+++ modules/netboot-builder.nix
@@ -1,11 +1,15 @@
 { pkgs, ... }: {
   mkNetbootFirmware = { uboot, configTxt, bootCmd }: pkgs.stdenv.mkDerivation {
     name = "rpi-netboot-${uboot.name}";
-    nativeBuildInputs = [ pkgs.ubootTools ];
+    # Use buildPackages to ensure the mkimage tool is runnable on your x86 server
+    nativeBuildInputs = [ pkgs.buildPackages.ubootTools ];
     BOOT_CMD = bootCmd;
     buildCommand = ''
       mkdir -p $out
-      ${pkgs.ubootTools}/bin/mkimage -A arm -O linux -T script -C none -n "Boot Script" -d $BOOT_CMD $out/boot.scr
+      # Use buildPackages to call the native binary
+      ${pkgs.buildPackages.ubootTools}/bin/mkimage -A arm -O linux -T script -C none -n "Boot Script" -d $BOOT_CMD $out/boot.scr
+      
+      # The rest of your paths are fine as is
       cp ${pkgs.raspberrypifw}/share/raspberrypi/boot/{bootcode.bin,start.elf,fixup.dat,*.dtb} $out/
       cp -r ${pkgs.raspberrypifw}/share/raspberrypi/boot/overlays $out/
       cp ${uboot}/u-boot.bin $out/kernel.img
